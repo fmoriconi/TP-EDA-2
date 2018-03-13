708f09ea12843e5424b034be21743d90a81ea18a
Unknown <matigonzalez@itba.edu.ar>
2018-03-13 09:52:54 -0300

    Falta main juan y testear

--- Fractal Mandelbrot/Mandelbrot.c
+++ Fractal Mandelbrot/Mandelbrot.c
@@ -12,8 +12,8 @@ void mandelrbot(double x0, double y0, double xf, double yf) {
 	int row, col;													//variables para indicar el pixel apuntado
 	double rad;
 
-	if ((abs(xf) + abs(x0)) >= (abs(yf) + abs(y0))) {
-		rad = ((abs(xf) + abs(x0)));
+	if ((abs(xf) + abs(x0)) >= (abs(yf) + abs(y0))) {				//busco el lado mas largo
+		rad = ((abs(xf) + abs(x0)) / 2);
 		}
 	else
 		rad = ((abs(yf) + abs(y0)) / 2);
@@ -23,13 +23,7 @@ void mandelrbot(double x0, double y0, double xf, double yf) {
 			double c_re = (col - XMAX / 2.0)*(rad * 2) / XMAX;			//calculo de la constante real    
 			double c_im = (row - YMAX / 2.0)*(rad * 2) / XMAX;			//calculo de la constante imaginaria
 			double x = 0, y = 0;
-			int iteration = 0;
-			while (x*x + y * y <= (rad*2) && iteration < MAX) {
-				double x_new = x * x - y * y + c_re;
-				y = 2 * x*y + c_im;
-				x = x_new;
-				iteration++;
-			}
+			recursive(x, y, &c_re, &c_im);
 			if (iteration < MAX)
 				al_put_pixel(col, row, al_map_rgb((unsigned char(interaction/4), (unsigned char)(interaction/4), (unsigned char)(interraction/4));
 			else
@@ -37,4 +31,15 @@ void mandelrbot(double x0, double y0, double xf, double yf) {
 		}
 	}
 
+}
+
+int recursive(double x, double y, double * c_re, double * c_im) {
+	if (x*x + y * y <= (rad * 2) && iteration < MAX)
+		return;
+	else {
+		double x_new = x * x - y * y + (*c_re);
+		y = 2 * x*y + (*c_im);
+		x = x_new;
+		return 1 + recursive(x, y, c_re, c_im);
+	}
 }
